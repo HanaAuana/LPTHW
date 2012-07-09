92a640338a312a94a62f75f35a33fc77010c9ba0
David Lim <dlim2010@gmail.com>
2012-07-08 20:35:40 -1000

    ex5

---
+++ ex5.py
@@ -0,0 +1,13 @@
+my_name = "Michael Lim"
+my_age = 20
+my_height = 69 #inches
+my_weight = 140 #pounds
+my_eyes = "Brown"
+my_hair = "Black"
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds." % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age+my_height+my_weight)
